8a61df333be8cd14f2c98fc9471b4fbdc6e63855
Saba pachulia <sabapachulia123@gmail.com>
2023-01-04 16:21:40 +0400

    changes

--- dist/app.js
+++ dist/app.js
@@ -8,6 +8,10 @@ const body_parser_1 = require("body-parser");
 const mongoose_1 = __importDefault(require("mongoose"));
 const all_1 = __importDefault(require("./routes/all"));
 const app = (0, express_1.default)();
+// 
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+// 
 app.use((0, body_parser_1.json)());
 // mongoose.set('strictQuery', false);
 const portStatus = process.env.PORT ? "https://sbs999.github.io" : "http://localhost:3000";
@@ -23,8 +27,9 @@ app.use((error, req, res, next) => {
     const message = error.message;
     res.status(400).json(message);
 });
-// const date = {year: new Date().getFullYear(),month: new Date().getMonth(),day: new Date().getDate(),hour: new Date().getHours(),minute: new Date().getMinutes()};
+// const date = {year: new Datje().getFullYear(),month: new Date().getMonth(),day: new Date().getDate(),hour: new Date().getHours(),minute: new Date().getMinutes()};
 // console.log(date);
+console.log('process.env.MONGODB_URL', process.env.MONGODB_URL);
 mongoose_1.default.connect('mongodb+srv://sabasaba123:sabasaba123@cluster0.5pewmdl.mongodb.net/shop?retryWrites=true&w=majority')
     .then(() => {
     const port = process.env.PORT || 8080;
--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,10 @@ import {json} from "body-parser";
 import mongoose from "mongoose";
 import router from "./routes/all";
 const app = express();
-
+// 
+import dotenv from "dotenv";
+dotenv.config();
+// 
 app.use(json());
 // mongoose.set('strictQuery', false);
 const portStatus = process.env.PORT ? "https://sbs999.github.io" : "http://localhost:3000";
@@ -22,9 +25,9 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     const message = error.message;
     res.status(400).json(message);
   })
-  // const date = {year: new Date().getFullYear(),month: new Date().getMonth(),day: new Date().getDate(),hour: new Date().getHours(),minute: new Date().getMinutes()};
+  // const date = {year: new Datje().getFullYear(),month: new Date().getMonth(),day: new Date().getDate(),hour: new Date().getHours(),minute: new Date().getMinutes()};
   // console.log(date);
-  
+  console.log('process.env.MONGODB_URL',process.env.MONGODB_URL);
   mongoose.connect('mongodb+srv://sabasaba123:sabasaba123@cluster0.5pewmdl.mongodb.net/shop?retryWrites=true&w=majority')
   .then(() => {
     const port = process.env.PORT || 8080;
